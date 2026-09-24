568b6af4a41c53735fe19f813edc9028ed4e23b4
PutluruLikhitha <likithaputluru29@gmail.com>
2026-09-24 09:50:19 +0530

    Update script..js

--- public/script..js
+++ public/script..js
@@ -1,4 +1,4 @@
-```javascript
+
 const API_URL = "https://46nfpdrf8e.execute-api.ap-south-1.amazonaws.com/dev1";
 
 // ================= REGISTER =================
@@ -103,4 +103,4 @@ if (loginForm) {
 
     });
 }
-```
+
